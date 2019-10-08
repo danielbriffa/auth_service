c37cf8651a9e1ed2106b1d95fc91b3889f73eec4
Daniel Briffa <daniel.briffa@brndwgn.com>
2019-10-08 14:39:04 +0200

    Change file name

--- database/migrations/2019_10_08_122150_add_phone_number_to_users_table.php
+++ database/migrations/2019_10_08_122150_add_phone_number_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UsersTableInsertColumnPhoneNumber extends Migration
+class AddPhoneNumberToUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,7 @@ class UsersTableInsertColumnPhoneNumber extends Migration
      */
     public function up()
     {
-        Schema::table('users', function($table)
-        {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('phone_number')->after('name');
         });
     }
@@ -26,7 +25,7 @@ class UsersTableInsertColumnPhoneNumber extends Migration
      */
     public function down()
     {
-        Schema::table('users', function ($table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('phone_number');
         });
     }
