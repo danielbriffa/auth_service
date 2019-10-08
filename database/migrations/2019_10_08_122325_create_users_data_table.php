7b712c026e13f2b088c7fb2280d280fc847c38f2
Daniel Briffa <daniel.briffa@brndwgn.com>
2019-10-08 14:39:19 +0200

    create new users date table

---
+++ database/migrations/2019_10_08_122325_create_users_data_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_data', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->json('user_data');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_data');
+    }
+}
