8a3eaa87525b1e556795f55584aee8135dd4a9f1
Daniel Briffa <daniel.briffa@brndwgn.com>
2019-10-10 17:55:21 +0200

    routes updates

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,32 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->group(
+    function()
+    {
 
+        //return information of logged in user
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+
+        //return all meta details of logged in user
+        Route::get('/user/meta', 'API\V1\UserDataController@showAll');
+
+        //return all values of specific key
+        Route::get('/user/meta/{key}', 'API\V1\UserDataController@show');
+
+        //delete all values from key - key included
+        Route::delete('/user/meta/{key}', 'API\V1\UserDataController@destroy');
+
+        //add field value to key
+        Route::put('/user/meta/{key}/{value}', 'API\V1\UserDataController@addValue');
+
+        //delete field value from key
+        Route::delete('/user/meta/{key}/{value}', 'API\V1\UserDataController@deleteValue');
+        
+    }
+);
 
 #Socialiate login routings
 Route::get('login/github', 'Auth\LoginController@redirectToProvider', 'github');
